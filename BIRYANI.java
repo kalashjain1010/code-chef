31b6ca674d2fe0e3f77576ca47c84e45c7db8e91
Kalash Jain <kalashjain54@gmail.com>
2022-09-06 14:48:39 +0530

    first commit

---
+++ BIRYANI.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class BIRYANI {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[2]; 
+        int k=0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[j]=sc.nextInt();
+                k = arr[0]*arr[1];
+            }
+            System.out.println(k);
+            
+        }
+    }
+}
